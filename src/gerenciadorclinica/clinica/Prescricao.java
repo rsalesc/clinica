813ac9d19feed1dc0728e5083ea73e22ff1163cb
Jeferson Lima <oi_je@hotmail.com>
2014-07-23 22:24:49 -0300

    %1 Adicionado metodo salvar na db nas classes Consulta, Exame, Prescricao

--- src/gerenciadorclinica/clinica/Prescricao.java
+++ src/gerenciadorclinica/clinica/Prescricao.java
@@ -1,8 +1,41 @@
 package gerenciadorclinica.clinica;
+import gerenciadorclinica.Entrada;;
 
-public class Prescricao {
+public class Prescricao extends Entrada{
 
-	public Prescricao() {
+	private String prescricao;
+	private String observacao;
+	
+	public Prescricao(String prescricao, int ID) {
+		super(ID);
+		this.prescricao = prescricao;
+		this.observacao = "";
 	}
 
+	public Prescricao(String prescricao, String observacao, int ID) {
+		super(ID);
+		this.prescricao = prescricao;
+		this.observacao = observacao;
+	}
+
+	public String getPrescricao() {
+		return prescricao;
+	}
+
+	public void setPrescricao(String prescricao) {
+		this.prescricao = prescricao;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	
+	public String toString(){
+		return ("Prescricao: " + prescricao + " Observações: " + observacao);
+	}
+	
 }
